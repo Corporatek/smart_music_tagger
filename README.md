23b094296701688299d244ea30a7ecf6b52fdbc6
corporatek <kagwunobi@my.uri.edu>
2019-03-29 01:56:56 -0400

    Figured out how to get the duration of an uploaded mp3 file. Added funcionality that knows how many times the tag file can be merged into the final mp3. Next step is to edit the concat function to create a new tag file that replays the tag the amount of times it will fit.

--- README.md
+++ README.md
@@ -10,3 +10,9 @@ npm install --save react-audio-player
 npm i sox-audio
 
 First I need to concat the users audio file with the standard munion tag. When that is done, add the output of the concat the the react audio player for streaming. In the final version, the concat will happen when a user uploads a file and the new file will be stored in the data base along with the original. The original copy can only be accessed when the buying user completes payment.
+
+New to convert uploaded audio file to 1 channel. To do this, follow these instructions:
+convert mp3 to a wav file => sox filename.mp3 filename.wav
+conver the new wav to a 1 channel wav => sox -c 1 filename.wav
+
+Or simply you can merge two files despite the properties of it => sox -M tagFile.wav beat.mp3
---
+++ src/SoxCommand.js
@@ -0,0 +1,81 @@
+var SoxCommand = require('sox-audio');
+var sox = require('sox');
+var fs = require('fs');
+
+// Returns errors and success messages
+var addStandardListeners = function(command) {
+	command.on('start', function(commandLine) {
+		console.log('Spawned sox with command ' + commandLine);
+	});
+
+	command.on('progress', function(progress) {
+	    console.log('Processing progress: ', progress);
+	});
+
+	command.on('error', function(err, stdout, stderr) {
+	    console.log('Cannot process audio: ' + err.message);
+	    console.log('Sox Command Stdout: ', stdout);
+	    console.log('Sox Command Stderr: ', stderr)
+	});
+
+	command.on('end', function() {
+	    console.log('Sox command succeeded!');
+	});
+};
+
+// A string of the tag and the uploaded file
+var fileNameList = ['/Users/newowner/Desktop/WDI/smart_music_tag/src/audiofiles/gunna.mp3', '/Users/newowner/Desktop/WDI/smart_music_tag/src/audiofiles/new_tag.mp3']
+
+// Gets the duration of the audio file and stores the number in the uploadDur variable
+
+var uploadDur = ""
+
+var getDuration = sox.identify(fileNameList[0], function(err, results) {
+
+    uploadDur =  results.duration
+    console.log(uploadDur)
+    
+  });
+
+console.log(uploadDur)
+
+//Below gets how many times the tag will fit into the the uploaded mp3 file
+var tagDur = 7.6
+
+var countLoop = function (a, b) {return a / b}
+
+loopCt = countLoop(uploadDur, tagDur)
+
+//Below adds the tag mp3 to an array the number of times it will fit into the track
+var tagArray = []
+
+for(i = 0; i < Math.floor(loopCt); i++) {
+	tagArray.push(fileNameList[1])
+}
+
+console.log(tagArray)
+
+// concat the files into a new one
+var concatFiles = function(fileNameList, outputPipe) {
+
+	var command = SoxCommand();
+	fileNameList.forEach(function addInput(fileName) {
+		command.input(fileName);
+	});
+	command.output(outputPipe)
+		.outputFileType('mp3')
+        .concat();
+
+	addStandardListeners(command);
+	command.run()
+	return command;
+};
+
+//the new output file's name. Need to add a method that creates a unique file name everytime an upload is ran.
+var outFileName = 'concatted_tag.mp3'
+var munionFileName = 'tagged_beat.mp3'
+
+//calling the concat function
+concatFiles(fileNameList, outFileName)
+mergeFiles(fileNameList, munionFileName)
+
